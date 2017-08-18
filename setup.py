7cf32d8934b964272124f8581712e9c5ca375d01
Ben Wiebe <ben.wiebe@windriver.com>
2017-08-18 10:40:58 -0400

    install: add Python version checking
    This commit adds a check to setup.py to verify that the correct version of
    Python is installed on the system. Currently, the minimum versions are 2.7.9
    and 3.4, however this may change as the module evolves. If the system is not
    using a version greater than or equal to these, then the installation will
    fail with a notice informing the user to upgrade their Python.
    
    Signed-off-by: Ben Wiebe <ben.wiebe@windriver.com>

--- setup.py
+++ setup.py
@@ -1,30 +1,37 @@
 from setuptools import setup
+import sys
+
+pyver = sys.version_info
+
+if pyver < (2,7,9) or (pyver > (3,0,0) and pyver < (3,4,0)):
+    print("Sorry, your Python version ({}.{}.{}) is not supported by helix!".format(pyver[0], pyver[1], pyver[2]))
+    sys.exit('Please upgrade to Python 2.7.9 or 3.4 and try again.')
 
 setup(
-	name='helix',
-	version='17.08.16',
-	description='Python library for Wind River\'s Helix Device Cloud',
-	author='Wind River Systems',
-	author_email='',
-	packages=['helix','helix._core','helix.test'],
-	install_requires=[
-		'paho-mqtt',
-		'requests',
-                'certifi',
-		'websocket-client'
-		],
-	maintainer='Paul Barrette',
-	maintainer_email='paul.barrette@windriver.com',
-	url='http://www.windriver.com/products/helix/',
-	license='',
-	classifiers=[
-		'Development Status :: 3 - Alpha',
-		'Intended Audience :: Developers',
-		'Natural Language :: English',
-		'Programming Language :: Python :: 2.7',
-		'Programming Language :: Python :: 2 :: Only',
-		'Operating System :: Microsoft :: Windows :: Windows 7',
-		'Operating System :: POSIX :: Linux',
-		'Topic :: Software Development :: Libraries :: Python Modules'
-		]
-	)
+    name='helix',
+    version='17.08.16',
+    description='Python library for Wind River\'s Helix Device Cloud',
+    author='Wind River Systems',
+    author_email='',
+    packages=['helix','helix._core','helix.test'],
+    install_requires=[
+        'paho-mqtt',
+        'requests',
+        'certifi',
+        'websocket-client'
+        ],
+    maintainer='Paul Barrette',
+    maintainer_email='paul.barrette@windriver.com',
+    url='http://www.windriver.com/products/helix/',
+    license='',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Operating System :: Microsoft :: Windows :: Windows 7',
+        'Operating System :: POSIX :: Linux',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+        ]
+    )
