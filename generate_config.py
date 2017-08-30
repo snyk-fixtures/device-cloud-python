485d5dbfb3e88464ec90f44069c9bd771f07ab36
Ben Astell <Ben.Astell@windriver.com>
2017-08-30 11:32:43 -0400

    generate-config: Updated generate config to support proxy configuration
    generate_config.py now has extra arguments for proxy configuration and the
    prompt has been expanded.
    
    Signed-off-by: Ben Astell <Ben.Astell@windriver.com>

--- generate_config.py
+++ generate_config.py
@@ -2,12 +2,12 @@
 
 '''
     Copyright (c) 2016-2017 Wind River Systems, Inc.
-    
+
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
     You may obtain a copy of the License at:
     http://www.apache.org/licenses/LICENSE-2.0
-    
+
     Unless required by applicable law or agreed to in writing, software  distributed
     under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
     OR CONDITIONS OF ANY KIND, either express or implied.
@@ -25,9 +25,18 @@ file_desc = "\nName of config file to write (eg. appname-connect.cfg) (Required)
 cloud_desc = "\nCloud host address (Required)"
 port_desc = "\nCloud port (eg. 1883/8883/443) (Required)"
 token_desc = "\nCloud token (Required)"
-do_validate_desc = "\n Validate SSL certificates (default true) (Optional)"
+do_validate_desc = "\nValidate SSL certificates (default true) (Optional)"
 no_validate_desc = "\nDo not validate SSL certificates (Optional)"
 cert_desc = "\nLocation of an ssl certificate bundle (If not set will use the bundle included with certifi instead) (Optional)"
+proxy_desc = "\nRoute all traffic through a proxy (default false) (Optional)"
+proxy_type_desc = "\nProxy type (eg. SOCKS4/SOCKS5/HTTP) (Optional)"
+proxy_host_desc = "\nProxy host address (Optional)"
+proxy_port_desc = "\nProxy port (Optional)"
+proxy_username_desc = "\nProxy username (Optional)"
+proxy_password_desc = "\nProxy password (Optional)"
+
+negatives = ["false", "f", "no", "n"]
+positives = ["true", "t", "yes", "y"]
 
 def generate():
     parser = argparse.ArgumentParser(description="Generate a connection config file. Give no arguments to run with prompt.")
@@ -37,6 +46,11 @@ def generate():
     parser.add_argument("-t", "--token", help=token_desc)
     parser.add_argument("-n", "--no-validate", help=no_validate_desc, dest='validate', action="store_false", default=True)
     parser.add_argument("-s", "--ssl-bundle", help=cert_desc)
+    parser.add_argument("--proxy-type", help=proxy_type_desc)
+    parser.add_argument("--proxy-host", help=proxy_host_desc)
+    parser.add_argument("--proxy-port", type=int, help=proxy_port_desc)
+    parser.add_argument("--proxy-username", help=proxy_username_desc)
+    parser.add_argument("--proxy-password", help=proxy_password_desc)
 
     args = parser.parse_args(sys.argv[1:])
     file_name = ""
@@ -62,6 +76,27 @@ def generate():
         else:
             missing.append("token")
 
+        if (args.proxy_type or args.proxy_host or args.proxy_port or
+                args.proxy_username or args.proxy_password):
+            config["proxy"] = {}
+            if args.proxy_type:
+                config["proxy"]["type"] = args.proxy_type
+            else:
+                missing.append("proxy type")
+            if args.proxy_host:
+                config["proxy"]["host"] = args.proxy_host
+            else:
+                missing.append("proxy host")
+            if args.proxy_port:
+                config["proxy"]["port"] = args.proxy_port
+            else:
+                missing.append("proxy port")
+
+            if args.proxy_username:
+                config["proxy"]["username"] = args.proxy_username
+            if args.proxy_password:
+                config["proxy"]["password"] = args.proxy_password
+
         if missing:
             print("Missing {}. Try again.".format(", ".join(missing)))
             return 1
@@ -78,8 +113,6 @@ def generate():
         if not file_name:
             print("File name is required.")
             return 1
-        if not os.path.splitext(file_name)[1]:
-            file_name += ".cfg"
 
         print(cloud_desc)
         temp = input("# ").strip()
@@ -112,9 +145,6 @@ def generate():
         print(do_validate_desc)
         temp = input("# ").strip()
         if temp:
-            negatives = ["false", "f", "no", "n"]
-            positives = ["true", "t", "yes", "y"]
-
             if temp.lower() in negatives:
                 config["validate_cloud_cert"] = False
             else:
@@ -128,6 +158,52 @@ def generate():
         temp = input("# ").strip()
         if temp:
             config["ca_bundle_file"] = temp
+
+        print(proxy_desc)
+        temp = input("# ").strip()
+        if temp and temp.lower() in positives:
+            config["proxy"] = {}
+
+            print(proxy_type_desc)
+            temp = input("# ").strip()
+            if temp:
+                config["proxy"]["type"] = temp
+            else:
+                print("Proxy type is required.")
+                return 1
+
+            print(proxy_host_desc)
+            temp = input("# ").strip()
+            if temp:
+                config["proxy"]["host"] = temp
+            else:
+                print("Proxy address is required.")
+                return 1
+
+            print(proxy_port_desc)
+            temp = input("# ").strip()
+            if temp:
+                if temp.isdigit():
+                    config["proxy"]["port"] = int(temp)
+                else:
+                    print("Proxy port must be an integer.")
+                    return 1
+            else:
+                print("Proxy port is required.")
+                return 1
+
+            print(proxy_username_desc)
+            temp = input("# ").strip()
+            if temp:
+                config["proxy"]["username"] = temp
+
+            print(proxy_password_desc)
+            temp = input("# ").strip()
+            if temp:
+                config["proxy"]["password"] = temp
+
+    if not os.path.splitext(file_name)[1]:
+        file_name += ".cfg"
 
     with open(file_name, "w+b") as config_file:
         config_file.write(json.dumps(config, indent=2, sort_keys=True).encode())
