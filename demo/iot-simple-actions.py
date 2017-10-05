9ae136f0b05ff5388f2a4fb091d833fab762b544
Leanne Clarke <Leanne.Clarke@windriver.com>
2017-10-05 16:25:01 -0400

    Client.py: Handler to set the default logging level
    Issue:#39
    
    Added new API call to set default log level.
    Fixed the path in the sample applications to the helix module.
    Also added diagnostic APIs ping and time.
    
    Signed-off-by: Leanne Clarke <Leanne.Clarke@windriver.com>

--- demo/iot-simple-actions.py
+++ demo/iot-simple-actions.py
@@ -25,6 +25,9 @@ import signal
 import sys
 from time import sleep
 
+head, tail = os.path.split(os.path.dirname(os.path.realpath(__file__)))
+sys.path.insert(0, head)
+
 import helix as iot
 from helix import osal
 
@@ -184,8 +187,8 @@ if __name__ == "__main__":
     # Deregister a previously registered action
     client.action_deregister("deregistered_action")
 
-    counter = 0
     while running and client.is_alive():
+
         # Wrap sleep with an exception handler to fix SIGINT handling on Windows
         try:
             sleep(1)
